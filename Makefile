acf6b42796f4a7713019d2f49288f93d36390aa8
Daniel Trnka <daniel.trnka@gmail.com>
2020-05-24 14:53:46 +0200

    Added m4load

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-CFLAGS=-Wall
+CFLAGS = -Wall
+BINS = hijack m4load
+SSH_DST ?= imx8
 
 hijack: hijack.c
+	aarch64-linux-gnu-gcc $^ -o $@ ${CFLAGS}
+
+m4load: m4load.c
 	aarch64-linux-gnu-gcc $^ -o $@ ${CFLAGS}
 
 modules:
@@ -10,6 +15,6 @@ modules:
 		make -C kernel_modules/find_proc  \
 	)
 
-upload: hijack modules
-	scp $(shell find -name "*.ko") hijack imx8:/tmp/
+upload: $(BINS) modules
+	scp $(shell find -name "*.ko") $(BINS) ${SSH_DST}:/tmp/
 
---
+++ m4load.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const uint32_t SRC_M4RCR = 0x3039000C;
+const uint32_t TCM = 0x7e0000;
+const uint32_t TCM_SIZE = 256 * 1024;
+const uint32_t M4_CTRL_MASK = 0b1111;
+const uint32_t M4_STOP      = 0b1100;
+const uint32_t M4_START     = 0b1010;
+
+uint8_t* map_mem(int fd, uint32_t addr, uint32_t size) {
+    uint32_t mask = getpagesize() - 1;
+    assert((size & mask) == 0);
+    uint8_t *mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr & ~mask);
+    if(mem == MAP_FAILED) {
+      fprintf(stderr, "Failed to map 0x%x: %s\n", addr, strerror(errno));
+      exit(1);
+    }
+    return mem + (addr & mask);
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+      fprintf(stderr, "Usage: %s firmware.bin\n", argv[0]);
+      exit(1);
+    }
+
+    int memfd = open("/dev/mem", O_RDWR | O_CLOEXEC);
+    if(memfd < 0) {
+      perror("open(/dev/mem): ");
+      exit(1);
+    }
+
+    // map SRC_M4RCR and TCM memory
+    volatile uint32_t *reg = (volatile uint32_t*) map_mem(memfd, SRC_M4RCR, getpagesize());
+    volatile uint8_t *tcm = map_mem(memfd, TCM, TCM_SIZE);
+    close(memfd);
+
+    int fd = open(argv[1], O_RDONLY);
+    if(!fd) {
+      perror("open");
+      exit(1);
+    }
+    off_t fsize = lseek(fd, 0, SEEK_END);
+    if(fsize >= TCM_SIZE) {
+      fprintf(stderr, "Firmware is too large. %lu > %u\n", fsize, TCM_SIZE);
+      exit(1);
+    }
+    lseek(fd, 0, SEEK_SET);
+
+    // stop m4 core
+    *reg = (*reg & ~M4_CTRL_MASK) | M4_STOP;
+
+    // load firmware
+    uint8_t firmware[TCM_SIZE];
+    size_t len = read(fd, firmware, sizeof(firmware));
+    close(fd);
+    for(size_t i = 0; i < len; i++) {
+      tcm[i] = firmware[i];
+    }
+
+    // start m4 core
+    *reg = (*reg & ~M4_CTRL_MASK) | M4_START;
+
+    return 0;
+}
